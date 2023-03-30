bc69c5c9e7fcb9ff8f5653ba6bbc3b5c365ffbca
adan llanos <adanllanos96@gmail.com>
2023-03-29 23:29:27 -0400

    test

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Project, Task, Laptop
+from .models import Project, Task, Laptop, Celular
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Task)
 admin.site.register(Laptop)
+admin.site.register(Celular)
+
